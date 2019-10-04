e16a92f767e5696cf2beaddbf6082cff39decce5
shouryaps <shourya4@gmail.com>
2019-10-04 19:10:35 +0530

    Basic structure for connector and custom exceptions

---
+++ finbox_bankconnect/connector.py
@@ -0,0 +1,8 @@
+def create_entity(link_id):
+    pass
+
+def get_link_id(entity_id):
+    pass
+
+def upload_file(=entity_id, file_obj, bank_name):
+    pass
---
+++ finbox_bankconnect/custom_exceptions.py
@@ -0,0 +1,5 @@
+class ExtractionFailedError:
+    pass
+
+class EntityNotFoundError:
+    pass
