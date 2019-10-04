76019e0befe6141cdbe085741616905a6ac757b6
shouryaps <shourya4@gmail.com>
2019-10-04 19:10:09 +0530

    Added global config variables

--- finbox_bankconnect/__init__.py
+++ finbox_bankconnect/__init__.py
@@ -1,2 +1,10 @@
 name = "finbox_bankconnect"
 api_key = None
+api_version = 'v1'
+max_retry_limit = 2
+base_url = 'https://portal.finbox.in'
+poll_timeout = 10 # seconds
+poll_lock_timeout = 10 # seconds
+poll_interval = 2 # seconds
+
+#TODO: Add authentication mode and also secret key + timestamp based authentication mode
