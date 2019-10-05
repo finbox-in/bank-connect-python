990a96e54826eb6f1b9281744b82f956b8d1a04e
shouryaps <shourya4@gmail.com>
2019-10-05 17:34:49 +0530

    Updated gitignore and init py

--- finbox_bankconnect/__init__.py
+++ finbox_bankconnect/__init__.py
@@ -4,7 +4,7 @@ api_version = 'v1'
 max_retry_limit = 2
 base_url = 'https://portal.finbox.in'
 poll_timeout = 10 # seconds
-poll_lock_timeout = 10 # seconds
 poll_interval = 2 # seconds
 
 #TODO: Add authentication mode and also secret key + timestamp based authentication mode
+from finbox_bankconnect.entity import Entity
