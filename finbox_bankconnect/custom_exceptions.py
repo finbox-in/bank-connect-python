2ee8179230dc3abc1a97856149cfa091cbd6ec60
shouryaps <shourya4@gmail.com>
2019-10-05 17:35:36 +0530

    Added custom exceptions

--- finbox_bankconnect/custom_exceptions.py
+++ finbox_bankconnect/custom_exceptions.py
@@ -1,5 +1,11 @@
-class ExtractionFailedError:
-    pass
+class ExtractionFailedError(Exception):
+    def __init__(self):
+        Exception.__init__(self, "Couldn't extract transactions from the given statement")
 
-class EntityNotFoundError:
-    pass
+class EntityNotFoundError(Exception):
+    def __init__(self):
+        Exception.__init__(self, "Couldn't find the entity with given entity_id")
+
+class ServiceTimeOutError(Exception):
+    def __init__(self):
+        Exception.__init__(self, "Couldn't reach the service")
