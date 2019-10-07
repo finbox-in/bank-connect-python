6384644de20910abb87be255beaca940ac516c77
shouryaps <shourya4@gmail.com>
2019-10-07 15:50:37 +0530

    Added test cases for get entity

--- tests.py
+++ tests.py
@@ -1,8 +1,43 @@
 import unittest
 import os
 import finbox_bankconnect as bc
+from finbox_bankconnect.custom_exceptions import EntityNotFoundError
 
-class TestTransactions(unittest.TestCase):
+class TestGetEntityEdgeCases(unittest.TestCase):
+
+    def setUp(self):
+        bc.api_key = os.environ['TEST_API_KEY']
+
+    def test_empty_entity_id(self):
+        exception_handled = False
+        try:
+            bc.Entity.get(entity_id = '')
+            bc.Entity.get(entity_id = None)
+        except ValueError:
+            exception_handled = True
+        self.assertEqual(exception_handled, True, "entity_id None and blank string case - not handled")
+
+    def test_invalid_entity_id(self):
+        exception_handled = False
+        try:
+            bc.Entity.get(entity_id = '123123123safasd')
+            bc.Entity.get(entity_id = 'c036e96dccae443c8f64b98ceeaa1578')
+        except ValueError:
+            exception_handled = True
+        self.assertEqual(exception_handled, True, "entity_id not a valid uuid4 (with hyphen) case - not handled")
+
+    def test_detail_not_found(self):
+        exception_handled = False
+        try:
+            entity = bc.Entity.get(entity_id = 'c036e96d-ccae-443c-8f64-b98ceeaa1578')
+            entity.get_transactions()
+            # assuming this uuid id doesn't exists in DB :P
+        except EntityNotFoundError:
+            exception_handled = True
+        self.assertEqual(exception_handled, True, "entity_id valid but not in DB case - not handled")
+
+
+class TestFetchTransactions(unittest.TestCase):
 
     def setUp(self):
         bc.api_key = os.environ['TEST_API_KEY']
@@ -10,13 +45,13 @@ class TestTransactions(unittest.TestCase):
         self.first_transaction = next(entity.get_transactions())
 
     def test_balance_exists(self):
-        self.assertIn("balance", self.first_transaction)
+        self.assertIn("balance", self.first_transaction, "balance not present in transaction")
 
     def test_transaction_type_exists(self):
-        self.assertIn("transaction_type", self.first_transaction)
+        self.assertIn("transaction_type", self.first_transaction, "balance not present in transaction")
 
     def test_date_exists(self):
-        self.assertIn("date", self.first_transaction)
+        self.assertIn("date", self.first_transaction, "balance not present in transaction")
 
 if __name__ == '__main__':
     unittest.main()
