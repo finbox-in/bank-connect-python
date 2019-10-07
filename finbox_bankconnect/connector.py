9d44348b78d009d31c4ad422317cfd7071f1cce4
shouryaps <shourya4@gmail.com>
2019-10-07 13:12:55 +0530

    Connector function to create entity, get link id, and upload file

--- finbox_bankconnect/connector.py
+++ finbox_bankconnect/connector.py
@@ -1,5 +1,8 @@
 import requests
 import finbox_bankconnect
+from custom_exceptions import ServiceTimeOutError, InvalidBankNameError
+from custom_exceptions import PasswordIncorrectError, UnparsablePDFError
+from custom_exceptions import FileProcessFailedError
 
 def get_progress_status(progress):
     for statement in progress:
@@ -8,18 +11,100 @@ def get_progress_status(progress):
     return "completed"
 
 def create_entity(link_id):
-    pass
+    url = "{}/bank-connect/{}/entity/".format(finbox_bankconnect.base_url, finbox_bankconnect.api_version)
+    headers = { 'x-api-key': finbox_bankconnect.api_key }
+    data = { 'link_id': link_id }
+
+    entity_id = None
+    response = None
+
+    retry_left = finbox_bankconnect.max_retry_limit
+    while retry_left:
+        response = requests.post(url, headers=headers, data=data)
+        if response.status_code == 200:
+            try:
+                entity_id = response.json()['entity_id']
+                break
+            except KeyError:
+                pass
+        retry_left -= 1
+    if entity_id is None:
+        #TODO: Log here
+        raise ServiceTimeOutError
+    return entity_id
 
 def get_link_id(entity_id):
-    pass
+    url = "{}/bank-connect/{}/entity/{}/".format(finbox_bankconnect.base_url, finbox_bankconnect.api_version, entity_id)
+    headers = { 'x-api-key': finbox_bankconnect.api_key }
 
-def upload_file(entity_id, file_obj, bank_name):
-    pass
+    link_id = None
+    response = None
+
+    retry_left = finbox_bankconnect.max_retry_limit
+    while retry_left:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            try:
+                link_id = response.json()['link_id']
+                break
+            except KeyError:
+                pass
+        retry_left -= 1
+    if link_id is None:
+        #TODO: Log here
+        raise ServiceTimeOutError
+    return link_id
+
+def upload_file(entity_id, file_obj, pdf_password, bank_name):
+    api_name = 'upload_file'
+    data = dict()
+    if bank_name is None:
+        api_name = 'bankless_upload'
+    else:
+        data['bank'] = bank_name
+    if entity_id is not None:
+        data['entity_id'] = entity_id
+    if pdf_password is not None:
+        data['pdf_password'] = pdf_password
+
+    url = "{}/bank-connect/{}/statement/{}/?identity_true".format(finbox_bankconnect.base_url, finbox_bankconnect.api_version, api_name)
+    headers = { 'x-api-key': finbox_bankconnect.api_key }
+    files = { 'file': file_obj }
+    response = None
+
+    retry_left = finbox_bankconnect.max_retry_limit
+    while retry_left:
+        response = requests.post(url, headers=headers, data=data, files=files)
+        if response.status_code == 200:
+            response = response.json()
+            try:
+                is_authentic = not response['is_fraud']
+                entity_id = response['entity_id']
+                identity = response['identity']
+                return is_authentic, entity_id, identity
+            except KeyError:
+                pass
+        elif response.status_code == 400:
+            response = response.json()
+            # check for invalid bank_name
+            if response.get('bank_name') is not None:
+                raise InvalidBankNameError
+            message = response.get('message')
+            if message is not None:
+                if message == "Password incorrect":
+                    raise PasswordIncorrectError
+                elif message == "PDF is not parsable":
+                    raise UnparsablePDFError
+                else:
+                    raise FileProcessFailedError
+        retry_left -= 1
+    #TODO: log here the response
+    raise ServiceTimeOutError
 
 def get_transactions(entity_id):
     url = "{}/bank-connect/{}/entity/{}/transactions/".format(finbox_bankconnect.base_url, finbox_bankconnect.api_version, entity_id)
     headers = { 'x-api-key': finbox_bankconnect.api_key }
-    response = requests.request("GET", url, headers=headers)
+    response = requests.get(url, headers=headers)
     if response.status_code == 404:
         return "not_found", None, None, None
     elif not response.status_code == 200:
@@ -42,7 +127,7 @@ def get_transactions(entity_id):
 def get_identity(entity_id):
     url = "{}/bank-connect/{}/entity/{}/identity/".format(finbox_bankconnect.base_url, finbox_bankconnect.api_version, entity_id)
     headers = { 'x-api-key': finbox_bankconnect.api_key }
-    response = requests.request("GET", url, headers=headers)
+    response = requests.get(url, headers=headers)
     if response.status_code == 404:
         return "not_found", None, None, None
     elif not response.status_code == 200:
@@ -64,7 +149,7 @@ def get_identity(entity_id):
 def get_accounts(entity_id):
     url = "{}/bank-connect/{}/entity/{}/accounts/".format(finbox_bankconnect.base_url, finbox_bankconnect.api_version, entity_id)
     headers = { 'x-api-key': finbox_bankconnect.api_key }
-    response = requests.request("GET", url, headers=headers)
+    response = requests.get(url, headers=headers)
     if response.status_code == 404:
         return "not_found", None, None
     elif not response.status_code == 200:
