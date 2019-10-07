d01b0639bdd72c6edceb34bc7437e40b11f5f90a
shouryaps <shourya4@gmail.com>
2019-10-07 17:41:14 +0530

    Handled edge case on util function

--- finbox_bankconnect/utils.py
+++ finbox_bankconnect/utils.py
@@ -2,7 +2,7 @@ def is_valid_uuid4(value):
     from uuid import UUID
     try:
         val = UUID(value, version=4)
-    except (ValueError, AttributeError):
+    except (ValueError, AttributeError, TypeError):
         return False
     # also check whether valid number of hyphens are present
     return value.count('-') == 4
