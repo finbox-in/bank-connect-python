3d6b9efbe7fba61f943d04fa0d10b0da1f3a3689
shouryaps <shourya4@gmail.com>
2019-10-10 12:06:24 +0530

    Reduced duplication in code coverage + switched back to deploy

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="finbox_bankconnect",
-    version="0.0.3",
+    version="0.0.4",
     author="FinBox",
     author_email="tech@finbox.in",
     description="Python library to use Finbox Bank Connect",
